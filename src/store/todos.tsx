87eda24dc1aa9e696c94b70f79910ff9a2cc3ada
“farah-jannat” <farahjannatmithila@gmail.com>
2024-12-02 13:47:11 +0600

    feat: update

--- src/store/todos.tsx
+++ src/store/todos.tsx
@@ -1,22 +1,53 @@
-import { createContext, ReactNode,useState } from "react";
+"use client"
+
+import {createContext, ReactNode,useContext,useState } from "react";
 
 
-export type Todo{
+export type Todo = {
     id:string;
     task:string;
     completed:boolean;
-    createdAt:Data
+    createdAt:Date;
 }
-export const todosContext = createContext(null)
-export const todosProvider = (children: {children:ReactNode}) =>{
+
+export type TodosContext ={
+    todos: Todo[];
+    handleAddTodo: (task:string) => void
+}
+
+export const todosContext = createContext<TodosContext | null>(null)
+
+export const TodosProvider = ({children} : {children:ReactNode}) =>{
+    const [todos, setTodos] = useState<Todo[]>([])
+
     const handleAddTodo = (task:string)=>{
         setTodos((prev)=>{
-            const newTodos ={
+            const newTodos:Todo[]=[
+                {
                 id: Math.random().toString(),
                 task,
-                complete:false,
+                completed:false,
                 createdAt:new Date()
-            }
-        })
+            },
+            ...prev 
+        ]
+        return newTodos;}
+    )
+   
+
     }
+
+    return (
+        <todosContext.Provider value={{todos, handleAddTodo}}>
+            {children}
+        </todosContext.Provider>
+    )
+}
+
+export function useTodos(){
+    const todosContextValue= useContext(todosContext)
+    if(!todosContextValue){
+        throw new Error('useTodos used outside of provider')
+    }
+    return todosContextValue
 }
