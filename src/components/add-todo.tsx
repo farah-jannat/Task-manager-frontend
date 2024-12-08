c62382fce5d45fa6f921499b14c9697b0d840779
“farah-jannat” <farahjannatmithila@gmail.com>
2024-12-08 18:23:09 +0600

    feat: update app-todo.tsx

--- src/components/add-todo.tsx
+++ src/components/add-todo.tsx
@@ -1,27 +1,66 @@
-'use client'
+"use client";
 
-import { useTodos } from '@/store/todos';
-import React, { FormEvent, useState } from 'react'
-
+import { useTodos } from "@/store/todos";
+import React, { FormEvent, useEffect, useState } from "react";
+import { CreateTask, GetAllTasks } from "@/api";
 const AddTodo = () => {
-    const {handleAddTodo} = useTodos()
+  const { handleTodos } = useTodos();
 
-    const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState("");
 
+  const fetchTodos = async () => {
+    try {
+      const { data } = await GetAllTasks();
+      console.log("all taks ", data);
+      return data
+    } catch (err) {
+      console.log(err);
+    };
+  };
+  // useEffect(()=>{
+  //   fetchTodos()
+  // },[])
 
-    const handleFormSubmit = (e:FormEvent<HTMLFormElement>)=>{
-        e.preventDefault()
-        handleAddTodo(todo)
-        setTodo('')
-
+  const handleAddTask = async () => {
+    const obj = {
+      taskName: todo,
+      isDone: false,
+    };
+    try {
+      const { success, message } = await CreateTask(obj);
+      if (success) {
+        // show scucess
+      } else {
+        // show err
+      }
+      setTodo("");
+      const allTodos = await fetchTodos();
+      handleTodos(allTodos)
+      console.log("all todos ", allTodos);
+    } catch (err) {
+      console.log(err);
     }
+  };
+
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // handleAddTodo(todo);
+    handleAddTask();
+    setTodo("");
+  };
 
   return (
     <form onSubmit={handleFormSubmit}>
-        <input type='text' placeholder='white your todo' name='' value={todo} onChange={(event) => setTodo(event.target.value)}/>
-        <button type='submit'>Add</button>    
+      <input
+        type="text"
+        placeholder="white your todo"
+        name=""
+        value={todo}
+        onChange={(event) => setTodo(event.target.value)}
+      />
+      <button type="submit">Add</button>
     </form>
-  )
-}
+  );
+};
 
-export default AddTodo
+export default AddTodo;
